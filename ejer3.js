1485f3a1fd87ed4333a7bca11ac26a4cc4890b9d
JSBHA <jhoan-071995@hotmail.com>
2025-02-11 22:47:11 -0500

    Challenge 3 is completed by creating a function that eliminates the extra step that is modified and is equal to the original

--- ejer3.js
+++ ejer3.js
@@ -1,40 +1,30 @@
 function findNaughtyStep(original, modified) {
+  if (original === modified) return "";
 
-    if (original === modified) return "" ;
-
-      // caso 3: es que original sea mayor que el modificado 
-      if (original.length > modified.length){
-     return [...original].find((v, i) => modified[i] !== v)
+  // Original sea mayor que el modificado
+  if (original.length > modified.length) {
+    return [...original].find((v, i) => modified[i] !== v);
   }
-  
-    // caso 4: es que modificado sea mayor que el original
-  
-  if (original.length < modified.length){
-    return [...modified].find((v, i) => original[i] !== v)
-  };
-       
+
+  // Modificado sea mayor que el original
+
+  if (original.length < modified.length) {
+    return [...modified].find((v, i) => original[i] !== v);
+  }
 }
-  
-  let original = 'abcd'
-  let modified = 'abcde'
- findNaughtyStep(original, modified) // 'e'
-  
-   original = 'stepfor'
-   modified = 'stepor'
-   console.log(findNaughtyStep(original, modified)) // 'f'
-  
-   original = 'abacde'
-   modified = 'abcde'
-  findNaughtyStep(original, modified) 
 
-  original = 'xxxxx'
-  modified = 'xxxx'
- findNaughtyStep(original, modified) 
+let original = "abcd";
+let modified = "abcde";
+findNaughtyStep(original, modified); // 'e'
 
+original = "stepfor";
+modified = "stepor";
+console.log(findNaughtyStep(original, modified)); // 'f'
 
+original = "abacde";
+modified = "abcde";
+findNaughtyStep(original, modified);
 
-  
-  
-
-  
-
+original = "xxxxx";
+modified = "xxxx";
+findNaughtyStep(original, modified);
