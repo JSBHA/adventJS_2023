11ebe59a5cb881a496a1fce2540030b41fcdce0a
JSBHA <jhoan-071995@hotmail.com>
2025-02-12 13:12:18 -0500

    Challenge 4 is completed by creating a function that removes characters that do not belong to the letter and orders the letters in reverse.

--- ejer4.js
+++ ejer4.js
@@ -1,30 +1,27 @@
 function decode(message) {
+  let result = []; // Este resultado se usará para almacenar las subcadenas dentro de paréntesis
+  let current = ""; // Esto mantiene la subcadena que estamos procesando actualmente
 
-    let result = []; // Este resultado se usará para almacenar las subcadenas dentro de paréntesis
-    let current = ''; // Esto mantiene la subcadena que estamos procesando actualmente
+  // Iteramos sobre cada carácter del mensaje
+  for (let char of message) {
+    // Si encontramos un parentesis de apertura
+    if (char === "(") {
+      //Apilamos el contenido actual y comenzamos una nueva subcadena
+      result.push(current);
+      current = ""; // Reiniciamos la subcadena actual para lo que esté dentro del paréntesis
 
-     // Iteramos sobre cada carácter del mensaje
-    for (let char of message) {
-
-        // Si encontramos un parentesis de apertura 
-        if (char === '('){
-            //Apilamos el contenido actual y comenzamos una nueva subcadena
-            result.push(current);
-            current = '' ; // Reiniciamos la subcadena actual para lo que esté dentro del paréntesis
-
-         // Si encontramos un parentesis de cierre 
-        }else if (char === ')'){
-            current = current.split('').reverse().join('');
-            let top = result.pop();  // Sacamos el último bloque de la pila
-            current = current + top; // Concatenamos el bloque anterior con el contenido procesado                             
-        }else{
-            current += char // Si es un carácter normal, lo añadimos al bloque actual
-        }
-        
-    } 
-    return current
+      // Si encontramos un parentesis de cierre
+    } else if (char === ")") {
+      current = current.split("").reverse().join("");
+      let top = result.pop(); // Sacamos el último bloque de la pila
+      current = current + top; // Concatenamos el bloque anterior con el contenido procesado
+    } else {
+      current += char; // Si es un carácter normal, lo añadimos al bloque actual
+    }
   }
+  return current;
+}
 
-  const a = decode('hola (odnum)')
-  const b = decode('(olleh) (dlrow)!')
-  const c = decode('sa(u(cla)atn)s')
+const a = decode("hola (odnum)");
+const b = decode("(olleh) (dlrow)!");
+const c = decode("sa(u(cla)atn)s");
